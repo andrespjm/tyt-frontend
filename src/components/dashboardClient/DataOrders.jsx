58931170c9d8e50d29243b101a49d7df509e01fb
unknown <omarramirez5876@gmail.com>
2022-08-30 19:33:15 -0500

    updating

--- src/components/dashboardClient/DataOrders.jsx
+++ src/components/dashboardClient/DataOrders.jsx
@@ -1,11 +1,28 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { getUserOrder } from '../../redux/actions';
 import { Menu } from './Menu';
 
 export const DataOrders = () => {
+  const {id} = useParams();
+  const dispatch = useDispatch();
+  const {redUser} = useSelector(state => state);
+  console.log(redUser)
+
+  useEffect(() => {
+    dispatch(getUserOrder(id))
+  }, [])
   return (
     <>
       <Menu />
       <div className="container mx-auto text-white">
-        Orders
+        Orders for {id}
+        <table>
+          <tr>
+            <th></th>
+          </tr>
+        </table>
       </div>
     </>
   )
