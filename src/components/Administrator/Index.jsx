86b46165bbff47fdd737871f9a636c105901ec47
MEugeV <eugeniavogt@yahoo.com.ar>
2022-09-02 06:10:52 -0300

    users disabled

--- src/components/Administrator/Index.jsx
+++ src/components/Administrator/Index.jsx
@@ -10,23 +10,28 @@ import { Create } from './ProductsCreate';
 import UserIcon from '@mui/icons-material/Group';
 import ReceiptIcon from '@mui/icons-material/Receipt';
 import FactoryIcon from '@mui/icons-material/Factory';
+import './Index.css';
 
 export default function DashBoard() {
 	return (
-		<Admin dataProvider={dataProvider}>
-			<Resource name='users' list={Users} edit={UserEdit} icon={UserIcon} />
-			<Resource
-				name='products'
-				list={Products}
-				create={Create}
-				icon={FactoryIcon}
-			/>
-			<Resource
-				name='purchases'
-				list={PurchaseList}
-				edit={PurchaseEdit}
-				icon={ReceiptIcon}
-			/>
-		</Admin>
+		<div>
+			<div>
+				<Admin dataProvider={dataProvider}>
+					<Resource name='users' list={Users} edit={UserEdit} icon={UserIcon} />
+					<Resource
+						name='products'
+						list={Products}
+						create={Create}
+						icon={FactoryIcon}
+					/>
+					<Resource
+						name='purchases'
+						list={PurchaseList}
+						edit={PurchaseEdit}
+						icon={ReceiptIcon}
+					/>
+				</Admin>
+			</div>
+		</div>
 	);
 }
