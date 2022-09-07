9fe69e89840db8917696f60c38e27afa961edb3d
Gustavo Pagano <gustavopagano03@gmail.com>
2022-09-07 01:27:48 -0300

    added format at button logout and order sales list

--- src/redux/reducers/sales.js
+++ src/redux/reducers/sales.js
@@ -5,7 +5,9 @@ const initialState = [];
 const salesData = (state = initialState, { type, payload }) => {
 	switch (type) {
 		case GET_SALES:
-			return payload;
+			return payload.sort((a, b) =>
+				a.orderId > b.orderId ? 1 : b.orderId > a.orderId ? -1 : 0
+			);
 		default:
 			return state;
 	}
